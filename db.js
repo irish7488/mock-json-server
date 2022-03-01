2607edc04df3a6a298f3b891f1d85b0a7d9318c7
Pankaj Raj <praj4936@gmail.com>
2022-03-01 21:30:23 +0530

    .

---
+++ db.js
@@ -0,0 +1,11 @@
+const tasks = require("./data/tasks");
+const users = require("./data/users");
+const todos = require("./data/todos");
+const products = require("./data/products")
+
+module.exports = {
+    tasks,
+    users,
+    todos,
+    products
+}
