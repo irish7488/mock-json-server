2c23d715a7bd39fedc4edf5380ab421e9d3d19a8
Pankaj Raj <praj4936@gmail.com>
2022-04-13 13:27:53 +0530

    add another db

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const tasks = require("./data/tasks");
 const users = require("./data/users");
 const todos = require("./data/todos");
-const products = require("./data/products")
+const products = require("./data/products");
+const userData = require("./data/userData");
 
 module.exports = {
     tasks,
     users,
     todos,
-    products
+    products,
+    userData
 }
